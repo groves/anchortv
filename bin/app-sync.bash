cd72dbbac914780f6497798b0537eedec1cfb73c
Charlie Groves <charliegroves@datavant.com>
2024-09-12 06:54:51 -0400

    Run the follow in the restart ssh session

--- bin/app-sync.bash
+++ bin/app-sync.bash
@@ -1,3 +1,2 @@
 rsync -rva ./*.py ./*.toml google-creds.json poetry.lock static templates anchortv:anchor
-ssh anchortv 'systemctl restart --user tv-schedule anchor-webapp'
-ssh anchortv 'journalctl --user -u tv-schedule -u anchor-webapp -f'
+ssh anchortv 'systemctl restart --user tv-schedule anchor-webapp && journalctl --user --follow -u tv-schedule -u anchor-webapp'
